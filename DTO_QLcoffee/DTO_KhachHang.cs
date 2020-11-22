b5ae008230bf190d657ced09e54bbc081dbe7923
hungpl <72348368+hungpl@users.noreply.github.com>
2020-11-22 15:38:54 +0700

    I just add two new class DTO_KhachHang and DTO_ThucDon

---
+++ DTO_QLcoffee/DTO_KhachHang.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO_QLcoffee
+{
+    public class DTO_KhachHang
+    {
+        private string maKh;
+        private string tenKh;
+        private string soDt;
+        private string email;
+        private string diaChi;
+
+        public string MaKh
+        {
+            get
+            {
+                return maKh;
+            }
+            set
+            {
+                maKh = value;
+            }
+        }
+        public string TenKh
+        {
+            get
+            {
+                return tenKh;
+            }
+            set
+            {
+                tenKh = value;
+            }
+        }
+        public string SoDt
+        {
+            get
+            {
+                return soDt;
+            }
+            set
+            {
+                soDt = value;
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return email;
+            }
+            set
+            {
+                email = value;
+            }
+        }
+        public string DiaChi
+        {
+            get
+            {
+                return diaChi;
+            }
+            set
+            {
+                diaChi = value;
+            }
+        }
+        public DTO_KhachHang(string maKh, string tenKh, string soDt, string email, string diaChi)
+        {
+            this.maKh = maKh;
+            this.tenKh = tenKh;
+            this.soDt = soDt;
+            this.email = email;
+            this.diaChi = diaChi;
+        }
+        public DTO_KhachHang() { }
+    }
+}
