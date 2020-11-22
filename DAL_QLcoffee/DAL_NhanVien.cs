6bb214c86d1b7f75fc859baf6b6c7ff5dcf4b2e6
tran-duc-minh <72349087+tran-duc-minh@users.noreply.github.com>
2020-11-22 16:09:03 +0700

    dataConnect

--- DAL_QLcoffee/DAL_NhanVien.cs
+++ DAL_QLcoffee/DAL_NhanVien.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace DAL_QLcoffee
 {
-    public class Class1
+    public class DAL_NhanVien : DataConnect
     {
     }
 }
---
+++ DAL_QLcoffee/DataConnect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAL_QLcoffee
+{
+    public class DataConnect
+    {
+        SqlConnection connection = new SqlConnection(@"Data Source=ADMIN;Initial Catalog=QuanLyCoffee;Integrated Security=True");
+    }
+}
