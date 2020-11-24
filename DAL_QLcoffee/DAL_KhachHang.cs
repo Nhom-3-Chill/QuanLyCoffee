fd0e41dcbdc487e4582652d1899e9ca36948501b
tran-duc-minh <72349087+tran-duc-minh@users.noreply.github.com>
2020-11-24 11:26:19 +0700

    danh sách khách hàng

--- DAL_QLcoffee/DAL_KhachHang.cs
+++ DAL_QLcoffee/DAL_KhachHang.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
+using DTO_QLcoffee;
+using System.Windows.Forms;
 
 namespace DAL_QLcoffee
 {
     public class DAL_KhachHang : DataConnect
     {
+        public DataTable DSKhacHang()
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "DanhSachKH";
+                command.Connection = connection;
+                DataTable table = new DataTable();
+                table.Load(command.ExecuteReader());
+                return table;
+            }
+            //catch (Exception a)
+            //{
+            //    MessageBox.Show(a.Message);
+            //}
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
--- DAL_QLcoffee/DataConnect.cs
+++ DAL_QLcoffee/DataConnect.cs
@@ -10,6 +10,6 @@ namespace DAL_QLcoffee
 {
     public class DataConnect
     {
-        SqlConnection connection = new SqlConnection(@"Data Source=ADMIN;Initial Catalog=QuanLyCoffee;Integrated Security=True");
+        protected SqlConnection connection = new SqlConnection(@"Data Source=(localdb)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\QuanLyCoffee.mdf;Integrated Security=True");
     }
 }
