f893e8040d1d3d70eeb2ae59f8c5907201060e7d
hungpl <72348368+hungpl@users.noreply.github.com>
2020-11-25 16:53:59 +0700

    I just write code in DAL_ThucDon class :))

--- DAL_QLcoffee/DAL_ThucDon.cs
+++ DAL_QLcoffee/DAL_ThucDon.cs
@@ -6,11 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using DTO_QLcoffee;
 
 namespace DAL_QLcoffee
 {
     public class DAL_ThucDon : DataConnect
     {
+        // Get Danh sach thuc don
         public DataTable DSThucDon()
         {
             try
@@ -24,15 +26,110 @@ namespace DAL_QLcoffee
                 tb.Load(scm.ExecuteReader());
                 return tb;
             }
-            //catch(Exception e)
-            //{
-            //    //MessageBox.Show(e.Message);
-            //    Console.WriteLine(e.Message);
-            //}
             finally
             {
                 connection.Close();
             }
         }
+
+        // Them mot thuc don moi
+        public bool LuuThucDon(DTO_ThucDon td)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand scm = new SqlCommand();
+                scm.CommandType = CommandType.StoredProcedure;
+                scm.CommandText = "InsertTD";
+                scm.Parameters.AddWithValue("@MaTD", td.MaTD);
+                scm.Parameters.AddWithValue("@TenTD", td.TenTD);
+                scm.Parameters.AddWithValue("@Gia",  td.Gia);
+                scm.Parameters.AddWithValue("@HinhAnh", td.HinhAnh);
+                scm.Connection = connection;
+                if (scm.ExecuteNonQuery() > 0)
+                    return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+
+        // Cap nhat thuc don
+        public bool SuaThucDon(DTO_ThucDon td)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand scm = new SqlCommand();
+                scm.CommandType = CommandType.StoredProcedure;
+                scm.CommandText = "UpdateTD";
+                scm.Parameters.AddWithValue("@TenTD", td.TenTD);
+                scm.Parameters.AddWithValue("@Gia", td.Gia);
+                scm.Parameters.AddWithValue("@HinhAnh", td.HinhAnh);
+                scm.Connection = connection;
+                if (scm.ExecuteNonQuery() > 0)
+                    return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+        public bool XoaThucDon(DTO_ThucDon td)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand scm = new SqlCommand();
+                scm.CommandType = CommandType.StoredProcedure;
+                scm.CommandText = "DeleteTD";
+                scm.Parameters.AddWithValue("@MaTD", td.MaTD);
+                scm.Connection = connection;
+                if (scm.ExecuteNonQuery() > 0)
+                    return true;
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+
+        // Tim kiem thuc don
+        public DataTable TimKiemThucDon(string tenTD)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand scm = new SqlCommand();
+                scm.CommandType = CommandType.StoredProcedure;
+                scm.CommandText = "SearchTD";
+                scm.Parameters.AddWithValue("@TenTD", tenTD);
+                scm.Connection = connection;
+                DataTable tb = new DataTable();
+                tb.Load(scm.ExecuteReader());
+                return tb;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
     }
 }
