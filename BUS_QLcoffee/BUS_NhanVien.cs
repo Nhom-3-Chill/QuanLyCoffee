f214822ccf10ec5cb98135553f937dcbd4572403
ps11960 <73158543+ps11960@users.noreply.github.com>
2020-11-25 18:07:21 +0700

    update bus va dal cua nhanvien

--- BUS_QLcoffee/BUS_NhanVien.cs
+++ BUS_QLcoffee/BUS_NhanVien.cs
@@ -11,7 +11,39 @@ namespace BUS_QLcoffee
 {
     public class BUS_NhanVien
     {
-
+        DAL_NhanVien dAL_nhanvien = new DAL_NhanVien();
+        public bool NVDangNhap(DTO_NhanVien nv)
+        {
+            return dAL_nhanvien.DangNhap(nv);
+        }
+        public bool NVQuenMatKhau(DTO_NhanVien nv)
+        {
+            return dAL_nhanvien.QuenMatKhau(nv);
+        }
+        public bool NVDoiMatKhau(DTO_NhanVien nv, string matkhaucu, string matkhaumoi)
+        {
+            return dAL_nhanvien.DoiMatKhau(nv,matkhaucu,matkhaumoi);
+        }
+        public DataTable DSNhanVien()
+        {
+            return dAL_nhanvien.DSNhanVien();
+        }
+        public bool LuuNhanVien(DTO_NhanVien nv)
+        {
+            return dAL_nhanvien.LuuNhanVien(nv);
+        }
+        public bool XoaNhanVien(string Manv)
+        {
+            return dAL_nhanvien.XoaNhanVien(Manv);
+        }
+        public bool SuaNhanVien(DTO_NhanVien nv)
+        {
+            return dAL_nhanvien.SuaNhanVien(nv);
+        }
+        public DataTable TimKiemNhanVien(string nv)
+        {
+            return dAL_nhanvien.TimKiemNhanVien(nv);
+        }
 
     }
 }
--- DAL_QLcoffee/DAL_NhanVien.cs
+++ DAL_QLcoffee/DAL_NhanVien.cs
@@ -1,12 +1,220 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using DTO_QLcoffee;
 
 namespace DAL_QLcoffee
 {
     public class DAL_NhanVien : DataConnect
     {
+        public bool DangNhap(DTO_NhanVien nv)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "DangNhap";
+                command.Connection = connection;
+                command.Parameters.AddWithValue("@email", nv.Email);
+                command.Parameters.AddWithValue("@matkhau", nv.MatKhau);
+                if (Convert.ToInt32(command.ExecuteScalar()) > 0)
+                {
+                    return true;
+                }
+            }
+            catch(Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+        public bool QuenMatKhau(DTO_NhanVien nv)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "QuenMatKhau";
+                command.Connection = connection;
+                command.Parameters.AddWithValue("@email", nv.Email);
+                if (Convert.ToInt32(command.ExecuteScalar()) > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+        public bool DoiMatKhau(DTO_NhanVien nv, string matkhaucu, string matkhaumoi)
+        {
+            try
+            {
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "DoiMatKhau";
+                command.Connection = connection;
+                command.Parameters.AddWithValue("@email", nv.Email);
+                command.Parameters.AddWithValue("@matkhaucu", matkhaucu);
+                command.Parameters.AddWithValue("@matkhaumoi", matkhaumoi);
+                if (Convert.ToInt32(command.ExecuteScalar()) > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show(x.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+        public DataTable DSNhanVien()
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "DanhSachNV";
+                command.Connection = connection;
+                DataTable table = new DataTable();
+                table.Load(command.ExecuteReader());
+                return table;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
+
+        public bool LuuNhanVien(DTO_NhanVien nv)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "LuuNV";
+                command.Parameters.AddWithValue("@manv", nv.MaNV);
+                command.Parameters.AddWithValue("@email", nv.Email);
+                command.Parameters.AddWithValue("@tennv", nv.TenNV);
+                command.Parameters.AddWithValue("@diachi", nv.DiaChi);
+                command.Parameters.AddWithValue("@vaitro", nv.VaiTro);
+                command.Parameters.AddWithValue("@tinhtrang", nv.TinhTrang);
+                command.Parameters.AddWithValue("@sdt", nv.SoDT);
+                command.Connection = connection;
+                if (command.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+
+            }
+            catch (Exception a)
+            {
+                MessageBox.Show(a.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+
+        public bool XoaNhanVien(string MaNhanVien)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "XoaNV";
+                command.Parameters.AddWithValue("@manv", MaNhanVien);
+                command.Connection = connection;
+                if (command.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception b)
+            {
+                MessageBox.Show(b.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+
+        public bool SuaNhanVien(DTO_NhanVien nv)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "SuaNV";
+                command.Parameters.AddWithValue("@manv", nv.MaNV);
+                command.Parameters.AddWithValue("@email", nv.Email);
+                command.Parameters.AddWithValue("@tennv", nv.TenNV);
+                command.Parameters.AddWithValue("@diachi", nv.DiaChi);
+                command.Parameters.AddWithValue("@vaitro", nv.VaiTro);
+                command.Parameters.AddWithValue("@tinhtrang", nv.TinhTrang);
+                command.Parameters.AddWithValue("@sdt", nv.SoDT);
+                command.Connection = connection;
+                if (command.ExecuteNonQuery() > 0)
+                {
+                    return true;
+                }
+            }
+            catch (Exception c)
+            {
+                MessageBox.Show(c.Message);
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return false;
+        }
+
+        public DataTable TimKiemNhanVien(string TenNV)
+        {
+            try
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand();
+                command.CommandType = CommandType.StoredProcedure;
+                command.CommandText = "TimKiemNV";
+                command.Parameters.AddWithValue("@tennv", TenNV);
+                command.Connection = connection;
+                DataTable table = new DataTable();
+                table.Load(command.ExecuteReader());
+                return table;
+            }
+            finally
+            {
+                connection.Close();
+            }
+        }
     }
 }
