9724c2626081c1c1e58a512c3c5f7e51018a11d2
tran-duc-minh <72349087+tran-duc-minh@users.noreply.github.com>
2020-12-08 11:20:41 +0700

    fixx

--- GUI_QLcoffee/frmHoaDon.Designer.cs
+++ GUI_QLcoffee/frmHoaDon.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHoaDon));
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.picboxRes = new System.Windows.Forms.PictureBox();
+            this.lblDatetime = new System.Windows.Forms.Label();
             this.lblTenNV = new System.Windows.Forms.Label();
             this.lblHoaDon = new System.Windows.Forms.Label();
             this.lblTongTien = new System.Windows.Forms.Label();
@@ -45,13 +47,11 @@
             this.btnThemmon = new System.Windows.Forms.Button();
             this.lblTenKH = new System.Windows.Forms.Label();
             this.lblTenMon = new System.Windows.Forms.Label();
-            this.lblDatetime = new System.Windows.Forms.Label();
-            this.picboxRes = new System.Windows.Forms.PictureBox();
             this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxRes)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).BeginInit();
             this.gpbThongTin.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.NumSoLuong)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picboxRes)).BeginInit();
             this.SuspendLayout();
             // 
             // groupBox1
@@ -65,20 +65,41 @@
             this.groupBox1.Controls.Add(this.btnThanhtoan);
             this.groupBox1.Controls.Add(this.dgvHoaDon);
             this.groupBox1.Controls.Add(this.gpbThongTin);
-            this.groupBox1.Location = new System.Drawing.Point(5, 2);
-            this.groupBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox1.Location = new System.Drawing.Point(4, 2);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.groupBox1.Size = new System.Drawing.Size(911, 630);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(683, 512);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
+            // 
+            // picboxRes
+            // 
+            this.picboxRes.Image = global::GUI_QLcoffee.Properties.Resources.iconfinder_Refresh_27873;
+            this.picboxRes.Location = new System.Drawing.Point(81, 457);
+            this.picboxRes.Margin = new System.Windows.Forms.Padding(2);
+            this.picboxRes.Name = "picboxRes";
+            this.picboxRes.Size = new System.Drawing.Size(44, 42);
+            this.picboxRes.TabIndex = 30;
+            this.picboxRes.TabStop = false;
+            this.picboxRes.Click += new System.EventHandler(this.picboxRes_Click_1);
+            // 
+            // lblDatetime
+            // 
+            this.lblDatetime.AutoSize = true;
+            this.lblDatetime.Location = new System.Drawing.Point(506, 188);
+            this.lblDatetime.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblDatetime.Name = "lblDatetime";
+            this.lblDatetime.Size = new System.Drawing.Size(0, 13);
+            this.lblDatetime.TabIndex = 29;
             // 
             // lblTenNV
             // 
             this.lblTenNV.AutoSize = true;
-            this.lblTenNV.Location = new System.Drawing.Point(773, 18);
+            this.lblTenNV.Location = new System.Drawing.Point(580, 15);
+            this.lblTenNV.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTenNV.Name = "lblTenNV";
-            this.lblTenNV.Size = new System.Drawing.Size(0, 17);
+            this.lblTenNV.Size = new System.Drawing.Size(0, 13);
             this.lblTenNV.TabIndex = 23;
             this.lblTenNV.Visible = false;
             // 
@@ -87,9 +108,10 @@
             this.lblHoaDon.AutoSize = true;
             this.lblHoaDon.Font = new System.Drawing.Font("Times New Roman", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHoaDon.ForeColor = System.Drawing.Color.Red;
-            this.lblHoaDon.Location = new System.Drawing.Point(385, 29);
+            this.lblHoaDon.Location = new System.Drawing.Point(289, 24);
+            this.lblHoaDon.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblHoaDon.Name = "lblHoaDon";
-            this.lblHoaDon.Size = new System.Drawing.Size(149, 38);
+            this.lblHoaDon.Size = new System.Drawing.Size(120, 31);
             this.lblHoaDon.TabIndex = 22;
             this.lblHoaDon.Text = "Hóa Đơn";
             // 
@@ -97,30 +119,31 @@
             // 
             this.lblTongTien.AutoSize = true;
             this.lblTongTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.lblTongTien.Location = new System.Drawing.Point(576, 581);
+            this.lblTongTien.Location = new System.Drawing.Point(432, 472);
+            this.lblTongTien.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTongTien.Name = "lblTongTien";
-            this.lblTongTien.Size = new System.Drawing.Size(98, 20);
+            this.lblTongTien.Size = new System.Drawing.Size(87, 17);
             this.lblTongTien.TabIndex = 20;
             this.lblTongTien.Text = "Tổng Tiền:";
             // 
             // txtTongtien
             // 
             this.txtTongtien.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.txtTongtien.Location = new System.Drawing.Point(695, 570);
-            this.txtTongtien.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtTongtien.Location = new System.Drawing.Point(521, 463);
+            this.txtTongtien.Margin = new System.Windows.Forms.Padding(2);
             this.txtTongtien.Multiline = true;
             this.txtTongtien.Name = "txtTongtien";
-            this.txtTongtien.Size = new System.Drawing.Size(133, 45);
+            this.txtTongtien.Size = new System.Drawing.Size(101, 37);
             this.txtTongtien.TabIndex = 21;
             // 
             // btnThanhtoan
             // 
             this.btnThanhtoan.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
             this.btnThanhtoan.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnThanhtoan.Location = new System.Drawing.Point(359, 565);
-            this.btnThanhtoan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnThanhtoan.Location = new System.Drawing.Point(269, 459);
+            this.btnThanhtoan.Margin = new System.Windows.Forms.Padding(2);
             this.btnThanhtoan.Name = "btnThanhtoan";
-            this.btnThanhtoan.Size = new System.Drawing.Size(144, 53);
+            this.btnThanhtoan.Size = new System.Drawing.Size(108, 43);
             this.btnThanhtoan.TabIndex = 19;
             this.btnThanhtoan.Text = "Thanh Toán";
             this.btnThanhtoan.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
@@ -131,12 +154,12 @@
             // 
             this.dgvHoaDon.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvHoaDon.Location = new System.Drawing.Point(71, 251);
-            this.dgvHoaDon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dgvHoaDon.Location = new System.Drawing.Point(53, 204);
+            this.dgvHoaDon.Margin = new System.Windows.Forms.Padding(2);
             this.dgvHoaDon.Name = "dgvHoaDon";
             this.dgvHoaDon.RowHeadersWidth = 51;
             this.dgvHoaDon.RowTemplate.Height = 24;
-            this.dgvHoaDon.Size = new System.Drawing.Size(773, 288);
+            this.dgvHoaDon.Size = new System.Drawing.Size(580, 234);
             this.dgvHoaDon.TabIndex = 13;
             // 
             // gpbThongTin
@@ -150,11 +173,11 @@
             this.gpbThongTin.Controls.Add(this.lblTenKH);
             this.gpbThongTin.Controls.Add(this.lblTenMon);
             this.gpbThongTin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.gpbThongTin.Location = new System.Drawing.Point(70, 79);
-            this.gpbThongTin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.gpbThongTin.Location = new System.Drawing.Point(52, 64);
+            this.gpbThongTin.Margin = new System.Windows.Forms.Padding(2);
             this.gpbThongTin.Name = "gpbThongTin";
-            this.gpbThongTin.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.gpbThongTin.Size = new System.Drawing.Size(773, 134);
+            this.gpbThongTin.Padding = new System.Windows.Forms.Padding(2);
+            this.gpbThongTin.Size = new System.Drawing.Size(580, 109);
             this.gpbThongTin.TabIndex = 2;
             this.gpbThongTin.TabStop = false;
             this.gpbThongTin.Text = "Thông Tin";
@@ -170,10 +193,10 @@
             "Trần Đức Thắng",
             "Trần Ngọc Diễm Quỳnh",
             "Phạm Linh Đan"});
-            this.cboTenKH.Location = new System.Drawing.Point(236, 82);
-            this.cboTenKH.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboTenKH.Location = new System.Drawing.Point(177, 67);
+            this.cboTenKH.Margin = new System.Windows.Forms.Padding(2);
             this.cboTenKH.Name = "cboTenKH";
-            this.cboTenKH.Size = new System.Drawing.Size(207, 28);
+            this.cboTenKH.Size = new System.Drawing.Size(156, 24);
             this.cboTenKH.TabIndex = 28;
             // 
             // cboTenMon
@@ -191,26 +214,27 @@
             "Trà chanh",
             "Ya - ua đá",
             "Ya - ua trái cây"});
-            this.cboTenMon.Location = new System.Drawing.Point(236, 28);
-            this.cboTenMon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.cboTenMon.Location = new System.Drawing.Point(177, 23);
+            this.cboTenMon.Margin = new System.Windows.Forms.Padding(2);
             this.cboTenMon.Name = "cboTenMon";
-            this.cboTenMon.Size = new System.Drawing.Size(253, 28);
+            this.cboTenMon.Size = new System.Drawing.Size(191, 24);
             this.cboTenMon.TabIndex = 27;
             // 
             // NumSoLuong
             // 
-            this.NumSoLuong.Location = new System.Drawing.Point(640, 30);
-            this.NumSoLuong.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.NumSoLuong.Location = new System.Drawing.Point(480, 24);
+            this.NumSoLuong.Margin = new System.Windows.Forms.Padding(2);
             this.NumSoLuong.Name = "NumSoLuong";
-            this.NumSoLuong.Size = new System.Drawing.Size(51, 26);
+            this.NumSoLuong.Size = new System.Drawing.Size(38, 23);
             this.NumSoLuong.TabIndex = 25;
             // 
             // linkKH
             // 
             this.linkKH.AutoSize = true;
-            this.linkKH.Location = new System.Drawing.Point(465, 90);
+            this.linkKH.Location = new System.Drawing.Point(349, 73);
+            this.linkKH.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.linkKH.Name = "linkKH";
-            this.linkKH.Size = new System.Drawing.Size(24, 20);
+            this.linkKH.Size = new System.Drawing.Size(23, 17);
             this.linkKH.TabIndex = 24;
             this.linkKH.TabStop = true;
             this.linkKH.Text = "...";
@@ -220,9 +244,10 @@
             // 
             this.lblSoLuong.AutoSize = true;
             this.lblSoLuong.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblSoLuong.Location = new System.Drawing.Point(560, 32);
+            this.lblSoLuong.Location = new System.Drawing.Point(420, 26);
+            this.lblSoLuong.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblSoLuong.Name = "lblSoLuong";
-            this.lblSoLuong.Size = new System.Drawing.Size(79, 20);
+            this.lblSoLuong.Size = new System.Drawing.Size(68, 17);
             this.lblSoLuong.TabIndex = 23;
             this.lblSoLuong.Text = "Số lượng:";
             // 
@@ -231,10 +256,10 @@
             this.btnThemmon.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
             this.btnThemmon.Image = global::GUI_QLcoffee.Properties.Resources.iconfinder_Add_27831;
             this.btnThemmon.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnThemmon.Location = new System.Drawing.Point(561, 70);
-            this.btnThemmon.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnThemmon.Location = new System.Drawing.Point(421, 57);
+            this.btnThemmon.Margin = new System.Windows.Forms.Padding(2);
             this.btnThemmon.Name = "btnThemmon";
-            this.btnThemmon.Size = new System.Drawing.Size(162, 60);
+            this.btnThemmon.Size = new System.Drawing.Size(122, 49);
             this.btnThemmon.TabIndex = 22;
             this.btnThemmon.Text = "Thêm Món";
             this.btnThemmon.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
@@ -247,9 +272,10 @@
             // 
             this.lblTenKH.AutoSize = true;
             this.lblTenKH.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTenKH.Location = new System.Drawing.Point(69, 90);
+            this.lblTenKH.Location = new System.Drawing.Point(52, 73);
+            this.lblTenKH.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTenKH.Name = "lblTenKH";
-            this.lblTenKH.Size = new System.Drawing.Size(139, 20);
+            this.lblTenKH.Size = new System.Drawing.Size(119, 17);
             this.lblTenKH.TabIndex = 1;
             this.lblTenKH.Text = "Tên Khách Hàng:";
             // 
@@ -257,38 +283,21 @@
             // 
             this.lblTenMon.AutoSize = true;
             this.lblTenMon.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTenMon.Location = new System.Drawing.Point(69, 34);
+            this.lblTenMon.Location = new System.Drawing.Point(52, 28);
+            this.lblTenMon.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblTenMon.Name = "lblTenMon";
-            this.lblTenMon.Size = new System.Drawing.Size(79, 20);
+            this.lblTenMon.Size = new System.Drawing.Size(68, 17);
             this.lblTenMon.TabIndex = 0;
             this.lblTenMon.Text = "Tên Món:";
             // 
-            // lblDatetime
-            // 
-            this.lblDatetime.AutoSize = true;
-            this.lblDatetime.Location = new System.Drawing.Point(674, 232);
-            this.lblDatetime.Name = "lblDatetime";
-            this.lblDatetime.Size = new System.Drawing.Size(0, 17);
-            this.lblDatetime.TabIndex = 29;
-            // 
-            // picboxRes
-            // 
-            this.picboxRes.Image = global::GUI_QLcoffee.Properties.Resources.iconfinder_Refresh_27873;
-            this.picboxRes.Location = new System.Drawing.Point(108, 563);
-            this.picboxRes.Name = "picboxRes";
-            this.picboxRes.Size = new System.Drawing.Size(59, 52);
-            this.picboxRes.TabIndex = 30;
-            this.picboxRes.TabStop = false;
-            this.picboxRes.Click += new System.EventHandler(this.picboxRes_Click_1);
-            // 
             // frmHoaDon
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(928, 633);
+            this.ClientSize = new System.Drawing.Size(696, 514);
             this.Controls.Add(this.groupBox1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmHoaDon";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản Lý Hóa Đơn";
@@ -296,11 +305,11 @@
             this.Load += new System.EventHandler(this.frmHoaDon_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxRes)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).EndInit();
             this.gpbThongTin.ResumeLayout(false);
             this.gpbThongTin.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.NumSoLuong)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picboxRes)).EndInit();
             this.ResumeLayout(false);
 
         }
