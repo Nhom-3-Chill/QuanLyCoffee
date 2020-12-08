d4c1c13e757c6a0336c3853f1f84ec9836f3c40a
ps11960 <73158543+ps11960@users.noreply.github.com>
2020-12-08 11:24:14 +0700

    Revert "test commit"
    This reverts commit f06a7d4c1356ea6b22a8535b021323065b3bc9aa.

--- GUI_QLcoffee/frmThongKe.Designer.cs
+++ GUI_QLcoffee/frmThongKe.Designer.cs
@@ -34,8 +34,8 @@
             this.dgvThongKe = new System.Windows.Forms.DataGridView();
             this.btnThongKe = new System.Windows.Forms.Button();
             this.gpbThongTin = new System.Windows.Forms.GroupBox();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtpKetthuc = new System.Windows.Forms.DateTimePicker();
+            this.dtpBatDau = new System.Windows.Forms.DateTimePicker();
             this.lblKetThuc = new System.Windows.Forms.Label();
             this.lblBatDau = new System.Windows.Forms.Label();
             this.gpbNhanvien.SuspendLayout();
@@ -48,9 +48,10 @@
             this.lblThongKe.AutoSize = true;
             this.lblThongKe.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
             this.lblThongKe.ForeColor = System.Drawing.Color.Red;
-            this.lblThongKe.Location = new System.Drawing.Point(369, 27);
+            this.lblThongKe.Location = new System.Drawing.Point(277, 22);
+            this.lblThongKe.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblThongKe.Name = "lblThongKe";
-            this.lblThongKe.Size = new System.Drawing.Size(153, 36);
+            this.lblThongKe.Size = new System.Drawing.Size(127, 29);
             this.lblThongKe.TabIndex = 1;
             this.lblThongKe.Text = "Thống Kê";
             // 
@@ -61,31 +62,33 @@
             this.gpbNhanvien.Controls.Add(this.gpbThongTin);
             this.gpbNhanvien.Controls.Add(this.lblThongKe);
             this.gpbNhanvien.Location = new System.Drawing.Point(11, 11);
-            this.gpbNhanvien.Margin = new System.Windows.Forms.Padding(2);
+            this.gpbNhanvien.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gpbNhanvien.Name = "gpbNhanvien";
-            this.gpbNhanvien.Padding = new System.Windows.Forms.Padding(2);
+            this.gpbNhanvien.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gpbNhanvien.Size = new System.Drawing.Size(676, 494);
             this.gpbNhanvien.TabIndex = 3;
             this.gpbNhanvien.TabStop = false;
             // 
             // dgvThongKe
             // 
+            this.dgvThongKe.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvThongKe.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
             this.dgvThongKe.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvThongKe.Location = new System.Drawing.Point(24, 230);
-            this.dgvThongKe.Margin = new System.Windows.Forms.Padding(2);
+            this.dgvThongKe.Location = new System.Drawing.Point(24, 246);
+            this.dgvThongKe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dgvThongKe.Name = "dgvThongKe";
             this.dgvThongKe.RowHeadersWidth = 51;
-            this.dgvThongKe.Size = new System.Drawing.Size(629, 197);
+            this.dgvThongKe.Size = new System.Drawing.Size(629, 223);
             this.dgvThongKe.TabIndex = 21;
             // 
             // btnThongKe
             // 
             this.btnThongKe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnThongKe.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnThongKe.Location = new System.Drawing.Point(243, 180);
-            this.btnThongKe.Margin = new System.Windows.Forms.Padding(2);
+            this.btnThongKe.Location = new System.Drawing.Point(243, 188);
+            this.btnThongKe.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnThongKe.Name = "btnThongKe";
-            this.btnThongKe.Size = new System.Drawing.Size(228, 43);
+            this.btnThongKe.Size = new System.Drawing.Size(171, 35);
             this.btnThongKe.TabIndex = 20;
             this.btnThongKe.Text = "Xem thống kê doanh thu";
             this.btnThongKe.UseVisualStyleBackColor = true;
@@ -93,43 +96,44 @@
             // 
             // gpbThongTin
             // 
-            this.gpbThongTin.Controls.Add(this.dateTimePicker2);
-            this.gpbThongTin.Controls.Add(this.dateTimePicker1);
+            this.gpbThongTin.Controls.Add(this.dtpKetthuc);
+            this.gpbThongTin.Controls.Add(this.dtpBatDau);
             this.gpbThongTin.Controls.Add(this.lblKetThuc);
             this.gpbThongTin.Controls.Add(this.lblBatDau);
-            this.gpbThongTin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gpbThongTin.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
             this.gpbThongTin.Location = new System.Drawing.Point(119, 68);
-            this.gpbThongTin.Margin = new System.Windows.Forms.Padding(2);
+            this.gpbThongTin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gpbThongTin.Name = "gpbThongTin";
-            this.gpbThongTin.Padding = new System.Windows.Forms.Padding(2);
+            this.gpbThongTin.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.gpbThongTin.Size = new System.Drawing.Size(419, 99);
             this.gpbThongTin.TabIndex = 3;
             this.gpbThongTin.TabStop = false;
             this.gpbThongTin.Text = "Thông Tin";
             // 
-            // dateTimePicker2
+            // dtpKetthuc
             // 
             this.dtpKetthuc.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.dtpKetthuc.Location = new System.Drawing.Point(153, 59);
             this.dtpKetthuc.Name = "dtpKetthuc";
-            this.dtpKetthuc.Size = new System.Drawing.Size(217, 22);
+            this.dtpKetthuc.Size = new System.Drawing.Size(217, 23);
             this.dtpKetthuc.TabIndex = 3;
             // 
-            // dateTimePicker1
+            // dtpBatDau
             // 
             this.dtpBatDau.Format = System.Windows.Forms.DateTimePickerFormat.Short;
             this.dtpBatDau.Location = new System.Drawing.Point(153, 25);
             this.dtpBatDau.Name = "dtpBatDau";
-            this.dtpBatDau.Size = new System.Drawing.Size(217, 22);
+            this.dtpBatDau.Size = new System.Drawing.Size(217, 23);
             this.dtpBatDau.TabIndex = 2;
             // 
             // lblKetThuc
             // 
             this.lblKetThuc.AutoSize = true;
             this.lblKetThuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.lblKetThuc.Location = new System.Drawing.Point(19, 79);
+            this.lblKetThuc.Location = new System.Drawing.Point(14, 64);
+            this.lblKetThuc.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblKetThuc.Name = "lblKetThuc";
-            this.lblKetThuc.Size = new System.Drawing.Size(157, 20);
+            this.lblKetThuc.Size = new System.Drawing.Size(134, 17);
             this.lblKetThuc.TabIndex = 1;
             this.lblKetThuc.Text = "Chọn ngày kết thúc:";
             // 
@@ -137,20 +141,21 @@
             // 
             this.lblBatDau.AutoSize = true;
             this.lblBatDau.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(254)));
-            this.lblBatDau.Location = new System.Drawing.Point(19, 37);
+            this.lblBatDau.Location = new System.Drawing.Point(14, 30);
+            this.lblBatDau.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblBatDau.Name = "lblBatDau";
-            this.lblBatDau.Size = new System.Drawing.Size(153, 20);
+            this.lblBatDau.Size = new System.Drawing.Size(132, 17);
             this.lblBatDau.TabIndex = 1;
             this.lblBatDau.Text = "Chọn ngày bắt đầu:";
             // 
             // frmThongKe
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(928, 639);
+            this.ClientSize = new System.Drawing.Size(696, 519);
             this.Controls.Add(this.gpbNhanvien);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmThongKe";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản Lý Thống Kê";
@@ -168,8 +173,8 @@
         private System.Windows.Forms.Label lblThongKe;
         private System.Windows.Forms.GroupBox gpbNhanvien;
         private System.Windows.Forms.GroupBox gpbThongTin;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dtpKetthuc;
+        private System.Windows.Forms.DateTimePicker dtpBatDau;
         private System.Windows.Forms.Label lblKetThuc;
         private System.Windows.Forms.Label lblBatDau;
         private System.Windows.Forms.Button btnThongKe;
